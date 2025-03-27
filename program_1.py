08e808f2dbe9671d12fe7ea29edc3e451dcfac8f
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-26 21:57:12 -0400

    Wrote programs

--- program_1.py
+++ program_1.py
@@ -1,16 +1,36 @@
+# Programmer: Austin Long
+# Date: 2025-03-26
+# Program: Item Counter
+
 # Program #1: Item Counter
-# Assume a file containing a series of names (as strings) is named names.txt 
+# Assume a file containing a series of names (as strings) is named names.txt
 # (Use the included example file names.txt) and exists on the computer's disk.
 # Write a program that displays the number of names that are stored in the file.
+
+import sys
 
 def count_file_lines():
     ######################
     # Add your code here #
     ######################
-    print('In the count_file_lines function')
+    print("In the count_file_lines function")
 
+    # Read file
+    count = 0
+    try:
+        with open("names.txt") as names_file:
+            for _ in names_file:
+                count += 1
+    except IOError as err:
+        print(f"An error occurred when reading the file: {err}", file=sys.stderr)
+        exit(err.errno)
+
+    # Display to user
+    print(
+        f"There {'are' if count != 1 else 'is'} {count} name{'s' if count != 1 else ''} in names.txt"
+    )
 
 
 # You don't need to change anything below this line:
-if __name__ == '__main__':
+if __name__ == "__main__":
     count_file_lines()
--- program_3.py
+++ program_3.py
@@ -1,19 +1,46 @@
+# Programmer: Austin Long
+# Date: 2025-03-26
+# Program: Average Numbers
+
 # Program #3: Average Numbers
 # Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk.
 # (please use the provided numbers.txt)
-# Write a program that reads all of the numbers stored in the file and calculates their total.  
+# Write a program that reads all of the numbers stored in the file and calculates their total.
 
-# The program should handle the following exceptions: 
-
+# The program should handle the following exceptions:
 # It should handle any IOError exceptions that are raised.
-# It should handle any ValueError exceptions that are raised when the items that are read from the file 
+# It should handle any ValueError exceptions that are raised when the items that are read from the file
 # are converted to a number.
+
+import sys
+
+
 def sum_numbers_from_file():
     ######################
     # Add your code here #
     ######################
-    print('In the sum_numbers_from_file function')
+    print("In the sum_numbers_from_file function")
+
+    grand_total = 0
+    try:
+        # Read lines and convert to integers
+        with open("numbers.txt") as names_file:
+            for line in names_file:
+                grand_total += int(line.strip())
+    except IOError as err:
+        # handle file exceptions
+        print(f"An error occurred when reading the file: ", file=sys.stderr)
+        exit(err.errno)
+    except ValueError as err:
+        # handle format exceptions
+        print(
+            "An error occurred when processing the file. One or more lines may not be integers."
+        )
+
+    # display output
+    print(f"The grand total is {grand_total}.")
+
 
 # You don't need to change anything below this line:
-if __name__ == '__main__':
+if __name__ == "__main__":
     sum_numbers_from_file()
